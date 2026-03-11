fd0adbc181aad427b0c35fdbc77cc46f08e0f020
Harrychen310125 <t114310125@ntut.org.tw>
2026-03-11 16:01:33 +0800

    Update Lab02.cpp

--- Lab02/Lab02.cpp
+++ Lab02/Lab02.cpp
@@ -1,1 +1,16 @@
-
+int findMax(const vector<int>& arr) {
+int  stepcount = 0; 
+// TODO: Add counts for initialization
+int max = arr[0];
+  stepcount ++;
+// TODO: Add counts for loop operations
+  stepcount++;
+for (int i = 1; i < arr.size(); i++) {
+// TODO: Add counts for comparison and assignment
+if (arr[i] > max) {
+max = arr[i];
+}
+}
+// TODO: Add counts for return
+return max;
+}
