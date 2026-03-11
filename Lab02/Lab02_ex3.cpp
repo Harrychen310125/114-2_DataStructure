7878124b80ac4b33a885bda4408b5eb1346e1878
Harrychen310125 <t114310125@ntut.org.tw>
2026-03-11 20:39:08 +0800

    Update Lab02_ex3.cpp

--- Lab02/Lab02_ex3.cpp
+++ Lab02/Lab02_ex3.cpp
@@ -1,1 +1,7 @@
-
+void copyArray(const vector<int>& source, vector<int>& dest) {
+int stepcount = 0;
+stepcounter ++;//初始化迴圈(step1)
+for (int i = 0; i < source.size(); i++) {
+dest[i] = source[i];
+}
+}
