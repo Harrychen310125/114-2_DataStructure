07045d58a7213022e8d4ee7f6e79ad5153f1d874
Harrychen310125 <t114310125@ntut.org.tw>
2026-03-11 22:13:17 +0800

    Update Lab02_ex3.cpp

--- Lab02/Lab02_ex3.cpp
+++ Lab02/Lab02_ex3.cpp
@@ -20,11 +20,14 @@ step++;//初始化迴圈(1step)
 for (int i = 0; i < arr.size(); i++) {
 step ++;//陣列存取(1step)
 if (arr[i] == target) {
-
+step ++;//比較(1step)
 count++;
 }
+step++;//1 step(i++)
 }
-step ++;//
+step ++;//跳出迴圈(1step)
+step ++;//return(1step)
 return count;
 }
-//
+//2(initial)+3*n(for loop operation)+2(跳出迴圈+return)
+//=3n+4
