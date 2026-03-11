d7c8ee0f3118d00d6ebe0d303910d267d84fbd26
Harrychen310125 <t114310125@ntut.org.tw>
2026-03-11 20:46:54 +0800

    Update Lab02_ex3.cpp

--- Lab02/Lab02_ex3.cpp
+++ Lab02/Lab02_ex3.cpp
@@ -3,6 +3,10 @@ int stepcount = 0;
 stepcounter ++;//初始化迴圈(step1)
 for (int i = 0; i < source.size(); i++) {
 stepcounter ++;//陣列存取(step1)
-dest[i] = source[i];
+dest[i] = source[i];  stepcount = stepcount+2;
+  //2step(陣列存取+assignment)
+stepcounter ++;//i++(step1)
 }
+stepcounter ++;//跳出迴圈(step1)
 }
+//1(initial)+4*n(
