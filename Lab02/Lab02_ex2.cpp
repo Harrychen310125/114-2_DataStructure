ddc8a539051aac522dd7bcf2ad34e79f760b26b7
Harrychen310125 <t114310125@ntut.org.tw>
2026-03-11 20:36:22 +0800

    Update Lab02_ex2.cpp

--- Lab02/Lab02_ex2.cpp
+++ Lab02/Lab02_ex2.cpp
@@ -8,6 +8,13 @@ for (int j = i + 1; j < arr.size(); j++) {
 stepcount ++;/迴圈條件判斷(1step)
 stepcount ++;//陣列存取(1step)
 cout << arr[i] << "," << arr[j] << endl;
+stepcount ++;//輸出字串(1step)
+stepcount ++;//j++(1step)
 }
+stepcount ++; //跳出迴圈(step)
+stepcount ++;//i++(1step)
 }
+//跳出迴圈(1step)
 }
+//1(initial)+4*n(outer for loop)*4*n(inner for loop)+1(跳出迴圈)
+//=16n^2+2
