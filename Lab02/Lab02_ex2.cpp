c08d5101cb89af5dd3d3e12ecdd8a666121e29c4
Harrychen310125 <t114310125@ntut.org.tw>
2026-03-11 19:50:38 +0800

    Update Lab02_ex2.cpp

--- Lab02/Lab02_ex2.cpp
+++ Lab02/Lab02_ex2.cpp
@@ -2,7 +2,8 @@ void printPairs(const vector<int>& arr) {
 int stepcount = 0;
 stepcount ++;//loop initialization (1step)
 for (int i = 0; i < arr.size(); i++) {
-  stepcount ++;//
+  stepcount ++;//迴圈條件判斷(1step)
+  
 // TODO: Add counts for inner loop
 for (int j = i + 1; j < arr.size(); j++) {
 // TODO: Add counts for printing operation
